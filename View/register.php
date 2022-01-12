2377d0184e631676161e1666969fa61565a6f1c3
Elouan <73824665+ByMSRT@users.noreply.github.com>
2022-01-12 22:18:24 +0100

    Merge pull request #1 from kevinguyodo/page-todolist
    Page todolist

---
+++ View/register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../View/static/register.css">
+    <title>Register - To do list</title>
+</head>
+
+<body>
+    <header>
+        <?= $content ?>
+    </header>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    ob_start();
+
+?>
+
+<div class="overlay">
+    <h1 class="title">To-do-list</h1>
+    <h2 class="desc">Que souhaitez-vous faire ?</h2>
+
+    <div class="button">
+        <a href="/Controllers/login.php"><button class="login">Login</button></a>
+        <a href="/Controllers/register.php"><button class="register">Register</button></a>
+    </div>
+</div>
+
+<?php
+    $content = ob_get_clean();
+?>
+
+<?php
+    ob_start();
+
+?>
+
+<div class="info">
+    <h1 class="title">Projet PHP</h1>
+    <h2 class="desc">Présenté par :</h2>
+    <p>Elouan DUMONT</p>
+    <p>Kévin GUYODO</p>
+    <p>Yasser YOUSSOUF</p>
+    <p>Enzo PINOT</p>
+    <a target="_blank" href="https://github.com/kevinguyodo/To_Do_Project_PHP">
+        <button class="git">Accès au GITHUB</button>
+    </a>
+</div>
+
+<?php
+    $footer = ob_get_clean();
+?>
+
+<?php
+    require "./View/homepage.php"
+?>
