04a7f1f39ac116ba7bf267ba3824c88fdfa8f408
kevinguyodo <kevin.guyodo@ynov.com>
2022-01-13 14:33:05 +0100

    update

---
+++ View/board.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./static/board.css">
+    <title>Board</title>
+</head>
+<body>
+    <header>
+        <div class="overlay">
+            <h1 class="title">To-do-list</h1>
+            <h2 class="desc">Que souhaitez-vous faire ?</h2>
+        </div>
+    </header>
+    <div class="add">
+        <a href="http://localhost:8000/View/project_to_do_list.php">
+            <input type="image" src="./IMG/plus_button.png" class="add_img" name="add_to_do">
+        </a>
+    </div>
+</body>
+</html>
