824cfd23cb1809ffc5be7ffcab141baea51680a9
Elouan <73824665+ByMSRT@users.noreply.github.com>
2022-01-14 22:27:11 +0100

    update

--- View/board.php
+++ View/board.php
@@ -6,12 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./static/board.css">
+    <link href="./static/all.css" rel="stylesheet">
     <title>Board</title>
 </head>
 
 <body>
     <header>
         <div class="overlay">
+            <div class="info_user">
+                <h3 class="name_user">**User**</h3>
+                <div class="icon">
+                    <i class="fas fa-user fa-2x"></i>
+                </div>
+                <div class="icon">
+                    <button type="submit" class="logout"><i class="fas fa-sign-out-alt fa-2x"></i></button>
+                </div>
+            </div>
             <h1 class="title">To-do-list</h1>
             <h2 class="desc">Bienvenue dans votre board</h2>
         </div>
