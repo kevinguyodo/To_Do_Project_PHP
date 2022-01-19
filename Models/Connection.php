1d8ea7f257be75ba44f0787c60b3fb34bcd54a38
Elouan <73824665+ByMSRT@users.noreply.github.com>
2022-01-19 18:46:14 +0100

    Update Connection.php

--- Models/Connection.php
+++ Models/Connection.php
@@ -10,17 +10,18 @@ class Connection {
      * @var type 
      */
     public $pdo;
+    
 
     /*
      * return in instance of the PDO object that connects to the SQLite database
      * @return \PDO
      */
     public function connect() {
-        if ($pdo == null) {
-            $pdo = new PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
-            // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if ($this->pdo == null) {
+            $this->pdo = new PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
-        return $pdo;
+        return $this->pdo;
     }
 
     function __construct() {}
